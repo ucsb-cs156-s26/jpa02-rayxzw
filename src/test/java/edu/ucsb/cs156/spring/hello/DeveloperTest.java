8e311d4106fca013ee0f3831513fd3f1545c5724
Raymond Xu <raymondxu@ucsb.edu>
2026-04-08 16:35:39 -0700

    rx - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,7 +25,8 @@ public class DeveloperTest {
         assertEquals("Raymond Xu", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+	assertEquals("rayxzw", Developer.getGithubId());
+    }
 }
