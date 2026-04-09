5863bcc843c5fa5a099cc5ee1aa0df5ad6c4e819
Raymond Xu <raymondxu@ucsb.edu>
2026-04-08 17:50:32 -0700

    rx - add tests for Team.hashCode(), equals(), toString()

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,53 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_true_same_ref() {
+        assert(team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_false_diff_class() {
+	String s = "adsf";
+        assert(!team.equals(s));
+    }
+    
+    @Test
+    public void equals_returns_true_same_name_and_members() {
+	Team t2 = new Team("test-team");
+        assert(team.equals(t2));
+    }
+
+    @Test
+    public void equals_returns_false_same_name_diff_members() {
+	Team t2 = new Team("test-team");
+	t2.addMember("asdf");
+        assert(!team.equals(t2));
+    }
+
+    @Test
+    public void equals_returns_false_diff_name_diff_members() {
+	Team t2 = new Team("asdf");
+	t2.addMember("1234");
+        assert(!team.equals(t2));
+    }
+
+    @Test
+    public void hashCode_eq_same_teams() {
+	Team t2 = new Team("test-team");
+        assertEquals(team.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_eq_mutate_bypass() {
+	Team t = new Team();
+	int result = t.hashCode();
+	int expectedResult = 1;
+        assertEquals(result, expectedResult);
+    }
 }
